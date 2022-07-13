2ce2d6c2161aa12647e00e004335c47042c5a3fa
Devesh-The Champ <madhukarkhade@gmail.com>
2022-07-14 03:34:39 +0530

    Commit

---
+++ db.js
@@ -0,0 +1,7 @@
+const users = [
+    {
+        email: "dvsh5@gmail.com",
+        password: "qwerty"
+    }
+]
+module.exports = users
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const auth = require('./route/auth')
+const app = express()
+app.use(express.json())
+app.use('/auth', auth)
+app.get('/', (req, res) => {
+    res.send('this is express server')
+})
+
+app.listen(7000, () => {
+    console.log("application is running")
+})
